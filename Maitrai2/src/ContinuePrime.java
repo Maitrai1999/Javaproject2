c1429174ec556c028f4eee52939a685e96bda7e6
unknown <maitraitumulury@gmail.com>
2022-02-20 17:31:25 +0530

    files

---
+++ Maitrai2/src/ContinuePrime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ContinuePrime {
+    	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1;
+        // while loop 
+        while(num<11){
+            // condition for continue
+            if (num%3==0){
+                num++;
+                continue;
+            }
+            // printing number
+            System.out.println("counting...."+num);
+            // incrementing the num
+            num++;
+        }
+
+
+	}
+
+	
+
+	}
+
+
---
+++ Maitrai2/src/Firstnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Firstnum {
+
+
+		public static void main(String[] args) {
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter first number:");
+			int a=s.nextInt();
+			System.out.println("Value of a is="+a);
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
